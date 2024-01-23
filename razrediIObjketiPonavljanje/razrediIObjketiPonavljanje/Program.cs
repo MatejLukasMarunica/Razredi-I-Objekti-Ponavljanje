b4a1418a4447d35423665fc414209aba3f5a5bb5
MatejLukasMarunica <145869520+MatejLukasMarunica@users.noreply.github.com>
2024-01-23 11:08:05 +0100

    Add files via upload

---
+++ razrediIObjketiPonavljanje/razrediIObjketiPonavljanje/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace razrediIObjketiPonavljanje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sport nogomet = new Sport("nogomet", true, 26);
+
+            Sport vaterpolo = new Sport("vaterpolo", true, 13);
+
+            Sport curling = new Sport("curling", false, 8);
+
+
+
+            Console.WriteLine(nogomet.ToString() + Environment.NewLine);
+
+            Console.WriteLine(vaterpolo.ToString() + Environment.NewLine);
+
+            Console.WriteLine(curling.ToString() + Environment.NewLine);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ razrediIObjketiPonavljanje/razrediIObjketiPonavljanje/Sport.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace razrediIObjketiPonavljanje
+{
+    internal class Sport
+    {
+        string naziv;
+        bool IgraLiSeLoptom;
+        int brojIgraca;
+
+        public void setnaziv(string naziv)
+        {
+            this.naziv = naziv;
+        }
+        public void setIgraLiSeLoptom(bool IgraLiSeLoptom)
+        {
+            this.IgraLiSeLoptom = IgraLiSeLoptom;
+        }
+        public void setbrojIgraca(int brojIgraca)
+        {
+            this.brojIgraca = brojIgraca;
+        }
+
+        public string getnaziv()
+        {
+            return this.naziv;
+        }
+        public bool getIgraLiSeLoptom()
+        {
+            return this.IgraLiSeLoptom;
+        }
+        public int getbrojIgraca()
+        {
+            return this.brojIgraca;
+        }
+
+        public Sport(string naziv, bool IgraLiSeLoptom, int brojIgraca)
+        {
+            this.naziv = naziv;
+            this.IgraLiSeLoptom = IgraLiSeLoptom;
+            this.brojIgraca = brojIgraca;
+        }
+
+        public Sport()
+        {
+        }
+
+        public override string ToString()
+        {
+            string ispis = "Naziv: " + this.getnaziv() + Environment.NewLine + " Dali se igra: " + this.getIgraLiSeLoptom() + Environment.NewLine + " Broj Igraca iznosi: " + this.getbrojIgraca();
+            return ispis;
+        }
+    }
+}
